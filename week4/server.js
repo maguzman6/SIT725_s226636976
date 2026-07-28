de72ade0c1256a067b7c7a4e6f3719464b900cb7
maguzman6 <s226636976@deakin.edu.au>
2026-07-28 14:31:06 +1000

    feat: add a simple docker compose file to run mongodb database, install mongoose and test connection

--- week4/server.js
+++ week4/server.js
@@ -1,9 +1,15 @@
 var express = require("express")
+const mongoose = require("mongoose");
 var app = express()
 
 app.use(express.static(__dirname + '/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect('mongodb://root:secretpassword123@127.0.0.1:27017');
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB!');
+});
 
 const cardList = [
     {
@@ -22,6 +28,7 @@ const cardList = [
 app.get('/api/projects', (req, res) => {
     res.json({ statusCode: 200, data: cardList, message: "Success" })
 })
+
 var port = process.env.port || 3000;
 
 var port = process.env.port || 3000;
