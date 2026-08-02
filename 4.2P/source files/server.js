2bd90f27cf3d5018dc34d002ba63a286d555a73d
maguzman6 <s226636976@deakin.edu.au>
2026-08-02 19:36:52 +1000

    feat: add default values for database parameters

--- 4.2P/source files/server.js
+++ 4.2P/source files/server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 
 const Coffee = require('./models/Coffee');
 
-mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@127.0.0.1:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}?authSource=admin`);
+mongoose.connect(`mongodb://${process.env.MONGO_USER || 'root'}:${process.env.MONGO_PASSWORD || 'secretpassword123'}@127.0.0.1:${process.env.MONGO_PORT || '27017'}/${process.env.MONGO_DATABASE || 'coffeeDB'}?authSource=admin`);
 mongoose.connection.on('connected', () => {
     console.log('Connected to MongoDB!');
 });
