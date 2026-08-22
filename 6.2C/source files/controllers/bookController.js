84516f7bea5fbf29bfc6509563540c8a0e6660ef
maguzman6 <s226636976@deakin.edu.au>
2026-08-22 22:37:25 +1000

    feat: update status codes and add error responses to previous endpoints

--- 6.2C/source files/controllers/bookController.js
+++ 6.2C/source files/controllers/bookController.js
@@ -4,20 +4,31 @@ const bookService = require('../services/bookService');
 // Controller uses the service to get data
 exports.getAllBooks = (req, res) => {
   const items = bookService.getAllBooks();
-  res.json({
-    status: 200,
-    data: items,
-    message: 'Books retrieved using service'
-  });
+  if (!items || items.length === 0) {
+    return res.status(404).json({
+      data: [],
+      message: 'Books not found'
+    });
+  } else {
+    res.status(200).json({
+      data: items,
+      message: 'Books retrieved using service'
+    });
+  }
 };
 
 // Use the service to get a book by ID
 exports.getBookById = (req, res) => {
   const items = bookService.getBookById(req.params.id);
-  res.json({
-    status: 200,
-    data: items,
-    message: 'Book retrieved using ID service'
-  });
+  if (!items || items.length === 0 || !items[0]) {
+    return res.status(404).json({
+      data: [],
+      message: 'Book not found'
+    });
+  } else {
+    res.status(200).json({
+      data: items,
+      message: 'Book retrieved using ID service'
+    });
+  }
 };
-
--- 6.2C/source files/controllers/luckyController.js
+++ 6.2C/source files/controllers/luckyController.js
@@ -4,10 +4,18 @@ const luckyService = require('../services/luckyService');
 // Controller uses the service to get data
 exports.getLuckyNumber = (req, res) => {
   const { number } = req.query;
-  const luckyNumber = luckyService.getLuckyNumber(number);
-  res.json({
-    status: 200,
-    data: { luckyNumber },
-    message: 'Lucky number retrieved'
-  });
+
+  // If number is missing or not a valid number, return 400 Bad Request
+  if (number === undefined || number === '' || isNaN(number)) {
+    return res.status(400).json({
+      data: null,
+      message: 'Invalid input: number parameter is required and must be numeric'
+    });
+  } else {
+    const luckyNumber = luckyService.getLuckyNumber(Number(number));
+    res.status(200).json({
+      data: { luckyNumber },
+      message: 'Lucky number retrieved'
+    });
+  }
 };
